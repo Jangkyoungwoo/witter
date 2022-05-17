94efb5eb3017ccc578e0b7fa1b5566775c472ace
Jangkyoungwoo <kyoungwoo990124@gmail.com>
2022-05-17 18:32:08 +0900

    초기 설정

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3aqJuXS-zKJT-KW2a8qzolnyOYGlZLd0",
+  authDomain: "witter-32ba9.firebaseapp.com",
+  projectId: "witter-32ba9",
+  storageBucket: "witter-32ba9.appspot.com",
+  messagingSenderId: "556241349518",
+  appId: "1:556241349518:web:63355fbadff8bda5ab52df",
+};
+
+export default firebase.initializeApp(firebaseConfig);
