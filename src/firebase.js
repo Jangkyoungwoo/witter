a6fa208049e2142bbff93b9c6ab979fcf52befae
Jangkyoungwoo <kyoungwoo990124@gmail.com>
2022-05-17 23:04:11 +0900

    라우터 설정

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3aqJuXS-zKJT-KW2a8qzolnyOYGlZLd0",
-  authDomain: "witter-32ba9.firebaseapp.com",
-  projectId: "witter-32ba9",
-  storageBucket: "witter-32ba9.appspot.com",
-  messagingSenderId: "556241349518",
-  appId: "1:556241349518:web:63355fbadff8bda5ab52df",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
