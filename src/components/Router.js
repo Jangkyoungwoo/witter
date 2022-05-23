a44d532394927ca4e89ec52c4a6ca5bbd310484d
Jangkyoungwoo <kyoungwoo990124@gmail.com>
2022-05-24 00:12:33 +0900

    로그인 인증

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,19 +1,17 @@
-import React, { useState } from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
       <Routes>
         {isLoggedIn ? (
           <>
-            <Route exact path="/" element={<Home />} />
+            <Route path="/" element={<Home />} />
           </>
         ) : (
-          <Route exact path="/" element={<Auth />} />
+          <Route path="/" element={<Auth />} />
         )}
       </Routes>
     </Router>
